118f6f3fde619f3eb85d176e6f3b49138ac377b3
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-30 23:40:42 +0100

    filtering caregories need to be finished design mostly done

--- index.js
+++ index.js
@@ -29,13 +29,50 @@ function adatMegjelenites(termekLista){
                 case 2:
                     megjelenitCardSubTitle.textContent = product[key]
                     break;
+                case 3:
+                    {
+                        let li = document.createElement('li')
+                        li.textContent = 'Ár: ' + product[key] + '$'
+                        li.style.fontWeight = 500;
+                        megjelenitCardTextUL.appendChild(li)
+                        break;
+                    }
+                case 4: {
+                    let li = document.createElement('li')
+                    li.textContent = 'Árcsökkenés mértéke: ' + product[key] + '%'
+                    megjelenitCardTextUL.appendChild(li)
+                    break;
+                }
+                case 5: {
+                    let li = document.createElement('li')
+                    li.textContent = 'Értékelés: ' + product[key] + '⭐'
+                    megjelenitCardTextUL.appendChild(li)
+                    break;
+                }
+                case 6: {
+                    let li = document.createElement('li')
+                    li.textContent = 'Raktáron: ' + product[key] + ' db'
+                    megjelenitCardTextUL.appendChild(li)
+                    break;
+                }
+                case 7: {
+                    let li = document.createElement('li')
+                    li.textContent = 'Márka: ' + product[key]
+                    li.style.fontWeight = 500;
+                    megjelenitCardTextUL.appendChild(li)
+                    break;
+                }
+                case 8: {
+                    let li = document.createElement('li')
+                    li.textContent = 'Kategória: ' + product[key]
+                    megjelenitCardTextUL.appendChild(li)
+                    break;
+                }
                 case 9:
                     megjelenitCardImage.src = product[key]
                     break;
                 default:
-                    let li = document.createElement('li')
-                    li.textContent = key + ': ' + product[key]
-                    megjelenitCardTextUL.appendChild(li)
+                    // data does not needed to be displayed
                     break;
             }
             i++
@@ -48,6 +85,34 @@ function adatMegjelenites(termekLista){
         megjelenitCard.appendChild(megjelenitCardBody)
         megjelenitCol.appendChild(megjelenitCard)
         document.getElementById('megjelenitRow').appendChild(megjelenitCol)
+    })
+}
+
+const categoriesSet = new Set()
+
+document.addEventListener('DOMContentLoaded', generateCategories)
+
+function generateCategories() {
+    fetch('/products.json')
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        data.products.forEach(product => {
+            for (let key in product){
+                if (key == 'category'){
+                    categoriesSet.add(product[key])
+                    console.log(categoriesSet)
+                }
+            }
+        })
+        categoriesSet.forEach(category => {
+            let option = document.createElement('option')
+            option.text = category
+            option.value = category
+            console.log(option)
+            document.getElementById('category-filter').appendChild(option)
+        })
     })
 }
 
